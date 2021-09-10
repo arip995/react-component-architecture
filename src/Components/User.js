79226fff9f3ee1c55751d3f4fa1bcaaa5a7f1eae
arip995 <arindampanda2002@gmail.com>
2021-09-10 20:27:46 +0530

    removed the mdb

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import App from '../App';
+import Table from "@material-ui/core/Table";
+import InputBase from '@material-ui/core/InputBase';
+import TableBody from "@material-ui/core/TableBody";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 import { useHistory } from 'react-router-dom';
 
 const User = (props) => {
